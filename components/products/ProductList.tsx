6cca698d0504b1a159c7ec300651701ff04c7920
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-10-15 15:08:39 -0300

    fix key error

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -14,7 +14,7 @@ export const ProductList: FC<Props> = ({ products }) => {
     return (
         <Grid container spacing={ 4 } >
             {
-                products.map(product => <ProductCard product={ product } key={ product._id } /> )
+                products.map((product, index) => <ProductCard product={ product } key={ index } /> )
             }
         </Grid>
     )
