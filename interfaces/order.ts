869eb79a656b2981288401e0221392c49b77874c
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-10-31 01:02:27 -0300

    fix errors

--- interfaces/order.ts
+++ interfaces/order.ts
@@ -1,10 +1,10 @@
-import { IUser } from "./";
+import { ISize, IUser } from "./";
 
 export interface IOrder {
-    _id:                string;
+    _id?:                string;
     user?:              IUser | string;
     orderItems:         Array<IOrderItem>;
-    shippingAdress:     ShippingAddress;
+    shippingAddress:     ShippingAddress;
     paymentResult?:     string;
 
     numberOfItems:      number;
@@ -19,11 +19,12 @@ export interface IOrder {
 export interface IOrderItem {
     _id:        string;
     title:      string;
-    size:       string;
+    size:       ISize;
     quantity:   number;
     slug:       string;
     image:      string;
     price:      number;
+    gender:     string;
 }
 
 export interface ShippingAddress {
