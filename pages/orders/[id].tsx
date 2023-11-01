66676f83ca2988ed93a1ef240594547edb29fb8b
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-11-01 01:40:29 -0300

    checks previosuly enter to the page

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,11 +1,36 @@
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
+import { getServerSession } from 'next-auth';
 import NextLink from 'next/link'
-import { Box, Card, CardContent, Divider, Grid, Typography, Button, Link, Chip } from '@mui/material';
-import { ShopLayout } from '../../components/layouts';
-import { CartList, OrdenSummary } from '../../components/cart';
-import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
 
-const OrderPage: NextPage = () => {
+import { 
+    Box, 
+    Card, 
+    CardContent, 
+    Divider, 
+    Grid, 
+    Typography, 
+    Link, 
+    Chip 
+} from '@mui/material';
+import { /* CreditCardOffOutlined, */ CreditScoreOutlined } from '@mui/icons-material';
+
+import { ShopLayout, CartList, OrdenSummary } from '../../components';
+
+import { authOptions } from '../api/auth/[...nextauth]';
+import { dbOrders } from '../../database';
+import { IOrder } from '../../interfaces';
+
+
+interface Props {
+    order: IOrder
+}
+
+/* The code is defining a React functional component called `OrderPage`. It is a TypeScript component
+that takes in a prop called `order` of type `IOrder`. */
+const OrderPage: NextPage<Props> = ({ order }) => {
+
+    console.log({ order });
+
     return (
         <ShopLayout title='Order summary 542' pageDescription='Order summary' >
             
@@ -94,6 +119,62 @@ const OrderPage: NextPage = () => {
         </ShopLayout>
     )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
+    
+    const { id = '' } = query;
+
+    const session: any = await getServerSession(req, res, authOptions);
+
+    /* This code block is checking if there is a valid session for the user. If there is no session, it
+    means the user is not authenticated. In that case, it redirects the user to the login page with
+    the `fromPage` query parameter set to the current order page. This allows the user to be
+    redirected back to the order page after logging in. The `permanent` property is set to `false`,
+    indicating that this redirect is temporary. */
+    if( !session ) {
+        return {
+            redirect: {
+                destination: `/auth/login?fromPage=/orders/${ id }`,
+                permanent: false,
+            }
+        };
+    };
+
+    const order = await dbOrders.getOrderById( id.toString() );
+
+    /* This code block is checking if the `order` variable is falsy, meaning it does not exist or is
+    empty. If the `order` does not exist, it means that the requested order does not exist in the
+    database. In this case, the code returns a redirect object with the `destination` property set
+    to `/orders/history`, indicating that the user should be redirected to the order history page.
+    The `permanent` property is set to `false`, indicating that this redirect is temporary. */
+    if( !order ) {
+        return {
+            redirect: {
+                destination: `/orders/history`,
+                permanent: false,
+            }
+        };
+    };
+
+    /* This code block is checking if the `user` property of the `order` object is not equal to the
+    `_id` property of the `user` object in the `session`. */
+    if( order.user !== session.user._id ) {
+        return {
+            redirect: {
+                destination: `/orders/history`,
+                permanent: false,
+            }
+        };
+    };
+
+    return {
+        props: {
+            order,
+        }
+    }
+}
+
 
 export default OrderPage
 
