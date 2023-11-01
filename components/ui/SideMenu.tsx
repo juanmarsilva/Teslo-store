5d78976ca17f9912a53516a05ff030a025aa44ef
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-11-01 01:38:44 -0300

    redirect to orders page

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -93,7 +93,7 @@ export const SideMenu = () => {
                                     <ListItemText primary={'Perfil'} />
                                 </ListItemButton>
 
-                                <ListItemButton>
+                                <ListItemButton onClick={() => navigateTo('/orders/history')} >
                                     <ListItemIcon>
                                         <ConfirmationNumberOutlined/>
                                     </ListItemIcon>
