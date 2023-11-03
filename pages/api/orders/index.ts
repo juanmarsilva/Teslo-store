0e8dbac694024f1d48265f0ac707ff48f3f7eeab
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-11-03 01:25:56 -0300

    round total number

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -63,6 +63,8 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     try {
         
+       /* The code is calculating the subtotal of the order by iterating over each item in the
+       `orderItems` array and multiplying the quantity of each item by its corresponding price. */
         const subtotal = orderItems.reduce((prevValue, current) => {
             const currentPrice = dbProducts.find( prod => prod.id === current._id )!.price;
 
@@ -80,6 +82,8 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         const userId = session.user._id;
 
         const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+
+        newOrder.total = Math.round( newOrder.total * 100 ) / 100;
 
         await newOrder.save();
 
